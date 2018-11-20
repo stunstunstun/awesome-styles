df987bba3edf10fb57ba8a095449fbd106d442f1
stunstunstun <wjdsupj@gmail.com>
2018-11-20 18:08:23 +0900

    Resolved GitHub Jobs layouts

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 require('../css/layout.scss')
+
+global.Flicking = new eg.Flicking("#flicking_wrapper")
+
+global.onClickLogo = function onClickLogo() {
+  document.getElementById('popup').style.display = 'block'
+}
+global.onClickClosePopup = function onClickClosePopup() {
+  document.getElementById('popup').style.display = 'none' 
+}
